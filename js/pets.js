10f79988a99ba4d634c68905418697f126765770
CodeCraftsman <142683382+FrontEndHighRoller@users.noreply.github.com>
2023-12-30 13:30:11 +0000

    Add files via upload

---
+++ js/pets.js
@@ -0,0 +1,37 @@
+const pets = [
+  {
+    name: "Joey",
+    type: "Dog",
+    breed: "Australian Shepherd",
+    age: 8,
+    photo: "../img/aussie.jpg",
+  },
+  {
+    name: "Jaz",
+    type: "Dog",
+    breed: "Dachshund",
+    age: 7,
+    photo: "../img/dachshund.jpg",
+  },
+  {
+    name: "Frank",
+    type: "Dog",
+    breed: "Golden Retriver",
+    age: 6,
+    photo: "../img/golden.jpg",
+  },
+  {
+    name: "Drake",
+    type: "Cat",
+    breed: "Persion",
+    age: 5,
+    photo: "../img/persian.jpg",
+  },
+  {
+    name: "Amayon",
+    type: "Dog",
+    breed: "Pug",
+    age: 4,
+    photo: "../img/pug.jpg",
+  },
+];
